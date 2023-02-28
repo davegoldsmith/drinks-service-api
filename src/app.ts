8f0c6b67ff31f7f7419e02fbe47f490f49bc025d
Dave Goldsmith <davegoldsmith@hotmail.com>
2023-02-28 09:59:53 +0000

    feat: Implement GET request for coffeelover  endpoint and test

--- src/app.ts
+++ src/app.ts
@@ -4,3 +4,5 @@ export const app = express();
 
 // handlers
 app.get("/", (req, res) => res.send("Welcome to the Drinks API!"));
+
+app.get("/coffeelover", (req, res) => res.send("I like coffee!"));
